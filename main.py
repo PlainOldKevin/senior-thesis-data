e2d9e2c188cfa7fdadb362d433482f2cb474186a
unknown <kevinconwaylax@gmail.com>
2023-04-12 15:27:17 -0400

    a ton of data

--- main.py
+++ main.py
@@ -2,16 +2,10 @@
 import pandas as pd
 from string import digits
 
-# Read csv files in
-data = pd.read_csv("sacbunt_data_since2012.csv") # All data
-# Positional data
-SSdata = pd.read_csv("SSdata.csv")
-Cdata = pd.read_csv("Cdata.csv")
-TwoBdata = pd.read_csv("TwoBdata.csv")
-CFdata = pd.read_csv("CFdata.csv")
+# Read csv file in
+data = pd.read_csv("sacbunt_data.csv")
 
-
-# Make dataframes
+# Make dataframe
 data_df = pd.DataFrame(data)
 
 # Remove digits in output
@@ -20,22 +14,35 @@ def remove_digits(string):
     return string.translate(remove_digits)
 
 # Function to print statistics from main data table
-def since2012_all():
+def bunting_data_overview():
     print("\n")
     # Initially, print size of data set
     print("Since 2012, there have been " + str(len(data)) + " situations in MLB (Regular season) that fit the given criteria:")
-    print("Ninth inning or later, <2 outs, RISP, game tied or batting team down 1 run, and a sac bunt occurs.")
+    print("Extra innings, home team batting, no outs, runner on second, game tied, and a sacrifice bunt is attempted.")
     print("This is some basic noting and analysis of the data.\n")
 
     # Batter involved in the most of these situations (laid down the bunt)
-    print("The batter who sac bunted the most in this scenario is " + data_df["Batter"].mode().to_string(index=False) + ", " +  " times\n")
+    print("The batter who sac bunted the most in this scenario is " + data_df["Batter"].mode().to_string(index=False) + ", 3 times\n")
 
     # WPA added avg
-    wpa_avg = str(round(data_df["WPA"].mean(), 2))
+    wpa_avg = str(round(data_df["WPA"].mean(), 3))
     print("The average WPA (win probability added) from the bunt in this scenario is " + wpa_avg + " (negligible)\n")
 
     # LI avg
     li_avg =  str(round(data_df["LI"].mean(), 2))
     print("The average LI (leverage index, the pressure the pitcher or batter saw during this play) in this scenario is " + li_avg + " (very high)\n")
 
-since2012_all()
+    # How many of these games did the home teamn wind up winning
+    wins = 0
+    for i in data["Rslt"].items():
+        if 'W' in i:
+            wins += 1
+    print("The number of wins the home team accrued where at least one of these situations occurred is " + str(wins) + " wins\n") 
+
+    print(data_df)
+
+    
+
+
+
+bunting_data_overview()
